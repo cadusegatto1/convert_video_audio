b17f832fd5e1aadab45c9331bf2bcaa5dfab738b
Carlos Segatto <cadusegatto1@gmail.com>
2024-06-21 01:52:37 -0300

    Update video_to_audio.py

--- video_to_audio.py
+++ video_to_audio.py
@@ -1,7 +1,11 @@
 # pip install
 import moviepy.editor
 
+# 🎥 Read video file
 video = moviepy.editor.VideoFileClip('video1.mp4')
 
+# 🔄 Convert video data to audio
 audio = video.audio
+
+# 💾 Save audio file
 audio.write_audiofile('audio_name.mp3')
